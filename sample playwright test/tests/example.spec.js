d46105fd4dc3940d4e6d92a26104f79aa29533eb
gholmes86 <galexholmes@gmail.com>
2023-08-29 11:41:25 -0500

    updating tests

--- sample playwright test/tests/example.spec.js
+++ sample playwright test/tests/example.spec.js
@@ -15,10 +15,24 @@ test('Verify answer retained after reload', async ({ page }) => {
   await expect(page.locator('[class="pathfinder-number-picker__item active"]')).toHaveText('5')
 });
 
-test('Verify user selects dropdown', async ({ page }) => {
+test('Verify user can select the dropdown', async ({ page }) => {
   await page.goto('https://webapp4.aaron.rios.azure.e3labs.net/survey/haslc/page/haslc-eligible');
   await expect(page).toHaveTitle(/resources/);
-  await page.selectOption('[placeholder="Select Income from the List"]', '$37,251 - $40,250')
+  await page.click('[placeholder="Select Income from the List"]')
+  await page.click('text=$37,251 - $40,250')
+  //unable to finish, the application itself goes to HTTP ERROR 502
+
+});
+
+test('Verify user non citizen', async ({ page }) => {
+  //unable to finish, the application itself goes to HTTP ERROR 502
+  await page.goto('https://webapp4.aaron.rios.azure.e3labs.net/survey/haslc/page/haslc-eligible');
+  await page.waitForLoadState("load")
+  await expect(page).toHaveTitle(/resources/);
+  await page.click('xpath=//*[@class="el-radio-button"]//*[@value="non-citizens"]')
+  await page.locator('button:text("Am I Eligible?")').click()
+  page.reload()
+  await expect(page.locator('[class="pathfinder-number-picker__item active"]')).sel()
 
 });
 
